45ddef8437bd64074ff5afcfef1e672f5c9127c5
Priyanka-6914 <londhepriyanka01@gmail.com>
2025-01-17 16:44:08 +0530

    Hi

---
+++ src/ex_02_Java_Basics/Lab001_printf.java
@@ -0,0 +1,11 @@
+package ex_02_Java_Basics;
+
+public class Lab001_printf {
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println(a);
+        System.out.print(a);
+        System.out.println();
+        System.out.printf("Your variable value is %d",a);
+    }
+}
